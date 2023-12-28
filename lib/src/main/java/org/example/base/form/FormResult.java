8ea8ccbaed2e469144442a94c4107599aabc0e4e
mxt <mxndtaylor@gmail.com>
2023-12-28 07:59:57 -0800

    fix see also link in javadoc

--- lib/src/main/java/org/example/base/form/FormResult.java
+++ lib/src/main/java/org/example/base/form/FormResult.java
@@ -7,7 +7,7 @@ import org.example.base.slot.SlotResult;
 
 /**
  *  Exists as convenience to collect all the assumptions we make about classes passed to Forms
- *  @see {@link org.example.base.slot.SlotResult}
+ *  @see org.example.base.slot.SlotResult
  */
 public interface FormResult<T extends FormResult<T>> extends Serializable {
     T copyWith(Map<String, ? extends SlotResult> map);
--- lib/src/main/java/org/example/base/slot/SlotResult.java
+++ lib/src/main/java/org/example/base/slot/SlotResult.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
  *  This class is here to collect everything that we assume about types passed as Slot type parameters,
  *  to provide better visibility of what types should be passed to begin with,
  *  and to provide easier way for end users to mark classes which they plan on creating Slots for
- *  @see {@link org.example.base.form.FormResult}
+ *  @see org.example.base.form.FormResult
  */
 public interface SlotResult extends Serializable {
 }
